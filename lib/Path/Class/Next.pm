0d2313ad5be0e90aed0a7111508c9a8af6c7c6bd
David Golden <dagolden@cpan.org>
2011-09-08 20:04:31 -0400

    initial experiment working, with tests

--- lib/Path/Class/Next.pm
+++ lib/Path/Class/Next.pm
@@ -7,6 +7,87 @@ package Path::Class::Next;
 
 # Dependencies
 use autodie 2.00;
+use Path::Class;
+use Scalar::Util qw/blessed/;
+
+#--------------------------------------------------------------------------#
+# class methods
+#--------------------------------------------------------------------------#
+
+sub new {
+  return bless { item_filter => sub {1} }, shift;
+}
+
+sub add_helper {
+  my ($class, $name, $coderef) = @_;
+  $class = ref $class || $class;
+  if ( ! $class->can($name) ) {
+    no strict 'refs';
+    *$name = sub { $_[0]->add_rule( $coderef ) };
+  }
+}
+
+#--------------------------------------------------------------------------#
+# object methods
+#--------------------------------------------------------------------------#
+
+sub add_rule {
+  my ($self, $rule) = @_;
+  # XXX rule must be coderef
+  if ( my $filter = $self->{item_filter} ) {
+    $self->{item_filter} = sub { $filter->(@_) && $rule->(@_) };
+  }
+  else {
+    $self->{item_filter} = $rule;
+  }
+  return $self;
+}
+
+sub iter {
+  my $self = shift;
+  my $opts =  ref($_[0])  && !blessed($_[0])  ? shift
+            : ref($_[-1]) && !blessed($_[-1]) ? pop : {};
+  my @queue = map { dir($_) } @_ ? @_ : '.';
+  my $filter = $self->{item_filter};
+  my $stash = $self->{stash};
+
+  return sub {
+    LOOP: {
+      my $item = shift @queue
+        or return;
+      local $_ = $item;
+      my ($interest, $prune) = $filter->($item, $stash);
+      push @queue, $item->children
+        if $item->is_dir && ! $prune;
+      return $item
+        if $interest;
+      redo LOOP;
+    }
+  };
+}
+
+sub all {
+  my $self = shift;
+  my $iter = $self->iter(@_);
+  my @results;
+  while ( my $item = $iter->() ) {
+    push @results, $item;
+  }
+  return @results;
+}
+
+#--------------------------------------------------------------------------#
+# common helpers
+#--------------------------------------------------------------------------#
+
+my %helpers = (
+  is_dir => sub { $_->is_dir },
+  is_file => sub { ! $_->is_dir },
+);
+
+while ( my ($k,$v) = each %helpers ) {
+  __PACKAGE__->add_helper( $k, $v );
+}
 
 1;
 
---
+++ t/basic.t
@@ -0,0 +1,54 @@
+use 5.006;
+use strict;
+use warnings;
+use Test::More 0.92;
+use Path::Class;
+use File::Temp;
+use File::pushd qw/pushd/;
+
+use lib 't/lib';
+use PCNTest;
+
+use Path::Class::Next;
+
+#--------------------------------------------------------------------------#
+
+{
+  my $td = make_tree(qw(
+    empty/
+    data/file1.txt
+  ));
+
+  my ($iter, @files);
+  my $rule = Path::Class::Next->new->is_file;
+
+  $iter = $rule->iter($td);
+
+  @files = ();
+  while ( my $f = $iter->() ) {
+    push @files, $f;
+  }
+
+  is( scalar @files, 1, "Iterator: one file");
+
+  @files = ();
+  @files = $rule->all($td);
+
+  is( scalar @files, 1, "All: one file");
+
+  $rule = Path::Class::Next->new->is_dir;
+  @files = ();
+  @files = $rule->all($td);
+
+  is( scalar @files, 3, "All: 3 directories");
+
+  my $wd = pushd($td);
+
+  @files = ();
+  @files = $rule->all();
+  is( scalar @files, 3, "All w/ cwd: 3 directories");
+
+}
+
+done_testing;
+# COPYRIGHT
